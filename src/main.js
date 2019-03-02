5759cdb0b18866c444e24592ca97559297779257
Alexandr <alex.mitin.psmu@gmail.com>
2019-03-02 19:14:52 +0500

    checkpoint

--- src/main.js
+++ src/main.js
@@ -2,7 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import FusionCharts from 'fusioncharts'
+import Charts from 'fusioncharts/fusioncharts.charts'
+import Widgets from 'fusioncharts/fusioncharts.widgets'
+import PowerCharts from 'fusioncharts/fusioncharts.powercharts'
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion'
+import VueFusionCharts from 'vue-fusioncharts'
 
+// Resolve the dependencies
+Charts(FusionCharts)
+PowerCharts(FusionCharts)
+Widgets(FusionCharts)
+FusionTheme(FusionCharts)
+
+// Globally register the components for project-wide use
+Vue.use(VueFusionCharts, FusionCharts)
 Vue.config.productionTip = false
 
 new Vue({
